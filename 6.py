ef39dcbecb32d8ae26e0c0b7c23b892228710b15
Ivan Kinder <ivankinder11@gmail.com>
2020-03-26 02:34:19 +0300

    Сделал 6 задание, надеюсь, я всё понял правильно :)

---
+++ 6.py
@@ -0,0 +1,14 @@
+def int_func(a):
+    return a.upper()
+
+
+words = input("Введите слова в любом регистре через пробел: ").split()
+for word in words:
+    if ord('a') <= ord(word[0]) <= ord('z'):
+        print (int_func(word[0]) + word[1:], ' ', end = '')
+    elif ord('A') <= ord(word[0]) <= ord('Z'):
+        print (int_func(word[0].lower()) + word[1:], ' ', end = '')
+    elif word[0].isdigit():
+        print (word, '', end = '')
+    else:
+        print ("Введите хотябы все буквы латинские в этом слове...")
